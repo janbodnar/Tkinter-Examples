46ab362261f29119784a17383791f4b124b82987
Jan Bodnar <jan.bodnar@gmail.com>
2019-04-18 23:33:25 +0200

    Create windows.py

---
+++ layout/windows.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""
+ZetCode Tkinter tutorial
+
+In this script, we use the grid
+manager to create a more complicated Windows
+layout.
+
+Author: Jan Bodnar
+Last modified: April 2019
+Website: www.zetcode.com
+"""
+
+from tkinter import Tk, Text, BOTH, W, N, E, S
+from tkinter.ttk import Frame, Button, Label, Style
+
+
+class Example(Frame):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.master.title("Windows")
+        self.pack(fill=BOTH, expand=True)
+
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(3, pad=7)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(5, pad=7)
+
+        lbl = Label(self, text="Windows")
+        lbl.grid(sticky=W, pady=4, padx=5)
+
+        area = Text(self)
+        area.grid(row=1, column=0, columnspan=2, rowspan=4,
+            padx=5, sticky=E+W+S+N)
+
+        abtn = Button(self, text="Activate")
+        abtn.grid(row=1, column=3)
+
+        cbtn = Button(self, text="Close")
+        cbtn.grid(row=2, column=3, pady=4)
+
+        hbtn = Button(self, text="Help")
+        hbtn.grid(row=5, column=0, padx=5)
+
+        obtn = Button(self, text="OK")
+        obtn.grid(row=5, column=3)
+
+
+def main():
+
+    root = Tk()
+    root.geometry("350x300+300+300")
+    app = Example()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
